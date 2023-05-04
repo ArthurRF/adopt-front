ad332755992c31b47f8a22e4b09c647822cc3890
ArthurRFr <arthur_fronza2011@hotmail.com>
2023-05-04 11:11:03 -0300

    feat: create the headerbar to test

---
+++ src/shared/components/header/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HeaderBar: React.FC = () => {
+  return (
+    <header className='bg-gray-800 p-4'>
+      <nav className='container mx-auto flex flex-wrap items-center justify-between'>
+        <div className='text-white'>ei</div>
+      </nav>
+    </header>
+  )
+}
+
+export { HeaderBar }
